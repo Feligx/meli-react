91d38c963303c5db7570bb121f03e56b4003e9a1
Felipe M <feligxgamer@gmail.com>
2022-04-23 14:48:29 -0500

    Update App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -11,7 +11,7 @@ const App = ()=>{
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Searchbar/>}/>
+                <Route path='/meli-react' element={<Searchbar/>}/>
                 <Route path='/items' element={<Results/>}/>
                 <Route path='/items/:id' element={<DetailedResult/>}/>
                 <Route path='*' element={<NotFound/>}/>
