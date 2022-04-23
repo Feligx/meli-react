3328165c27596d8840c7a11ca0c65f221a4e4e3d
Felipe M <feligxgamer@gmail.com>
2022-04-23 14:51:51 -0500

    [UPDATE] package and webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/'
+        publicPath: '/meli-react/'
     },
     mode: 'development',
     resolve: {
